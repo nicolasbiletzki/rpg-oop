f36fc8ef39e6b8e400706e17a3664effcff6edc4
Nicolas Biletzki <nicolasbiletzki@gmail.com>
2022-02-09 12:39:58 -0300

    Inciando projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp___POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/Entities/Arus.cs
@@ -0,0 +1,7 @@
+namespace CSharp___POO.src.Entities
+{
+    public class Arus
+    {
+        public string Name { get; set; }
+    }
+}
