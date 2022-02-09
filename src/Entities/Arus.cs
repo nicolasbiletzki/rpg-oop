64cadc31868330d55eb7247b895267f861220ccd
Nicolas Biletzki <nicolasbiletzki@gmail.com>
2022-02-09 12:50:55 -0300

    Criando Construtor Arus

--- src/Entities/Arus.cs
+++ src/Entities/Arus.cs
@@ -2,6 +2,17 @@ namespace CSharp___POO.src.Entities
 {
     public class Arus
     {
+        public Arus(string Name, int Level, string HeroType)
+        {
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+        }
+
+
         public string Name { get; set; }
+        public int Level { get; set; }
+        public string HeroType { get; set; }
+
     }
 }
